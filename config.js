d1b627ec7ccec118f55773f8fb87af8859a21951
Dindas <hello@thriv.pt>
2025-12-04 19:40:42 +0000

    Merge audit fixes1

---
+++ config.js
@@ -0,0 +1,3 @@
+// Google Vision API Configuration
+// Get your API key from: https://console.cloud.google.com/apis/credentials
+export const GOOGLE_VISION_API_KEY = 'AIzaSyBOaBsoOiR3rWclkqW3O1pDCfSdX7adyQs';
